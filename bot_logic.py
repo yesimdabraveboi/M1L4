84827ae55ebfd5fe92a5bb465d1ada7e62a2df95
yesimdabraveboi <160326226+yesimdabraveboi@users.noreply.github.com>
2024-03-09 20:29:53 +0700

    bot_logic.py

---
+++ bot_logic.py
@@ -0,0 +1,24 @@
+import random
+
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "Heads. That is a plus coin power, manager."
+    else:
+        return "Tails. You got no coin power gained, manager."
